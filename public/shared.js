a36a1b43a8221d85de5f9192c387bf9e028adfa1
Socks2109 <isaacyeohcj@gmail.com>
2023-05-07 22:06:11 -0700

    fixed menu icon issue

---
+++ public/shared.js
@@ -0,0 +1,33 @@
+/**
+ * Name: Isaac Yeoh & Nelson Chen
+ * Date: May 7th, 2023
+ * Section: CSE 154 AG
+ * This code is shared between all files. It toggles the
+ * view of the menu icon so that the menu bar expands or hides when
+ * the user clicks on it.
+ */
+
+"use strict";
+(function() {
+  window.addEventListener('load', init);
+
+  function init() {
+    qs(".menu").addEventListener("click", clickMenu);
+  }
+
+  /**
+   * Toggles the view of the menu bar
+   */
+  function clickMenu() {
+    qs(".menu-bar").classList.toggle("hidden");
+  }
+
+  /**
+   * Returns first element matching selector.
+   * @param {string} selector - CSS query selector.
+   * @returns {object} - DOM object associated selector.
+   */
+  function qs(selector) {
+    return document.querySelector(selector);
+  }
+})();
