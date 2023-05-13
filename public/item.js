345c527fe27967ad529cece5421f98078e358355
Nelson Chen <nelsonpc@uw.edu>
2023-05-13 12:50:02 -0700

    linked hotel name to item page

---
+++ public/item.js
@@ -0,0 +1,50 @@
+/**
+ * Name: Isaac Yeoh & Nelson Chen
+ * Date: May 7th, 2023
+ * Section: CSE 154 AG
+ * This code generates the images of the hotels and the name of
+ * the hotels in our home page. It will later contain the filter option
+ * and we will replace the src of the images with actual images
+ * in the near future.
+ */
+
+ "use strict";
+ (function() {
+
+   window.addEventListener('load', init);
+
+   /**
+    * initiates page upon load
+    */
+   function init() {
+     getHotelName();
+   }
+
+   function getHotelName() {
+    // Get the item ID from the URL query parameter
+    const urlParams = new URLSearchParams(window.location.search);
+    const hotelName = urlParams.get('hotel_nm');
+
+    // Display the corresponding item on the page
+    let h1 = qs('h1');
+    h1.textContent = hotelName.trim().split('%20').join(' ');
+   }
+
+  /**
+    * Returns the element that has the ID attribute with the specified value.
+    * @param {string} id - element ID.
+    * @returns {object} - DOM object associated with id.
+    */
+   function id(id) {
+     return document.getElementById(id);
+   }
+
+  /**
+   * Returns first element matching selector.
+   * @param {string} selector - CSS query selector.
+   * @returns {object} - DOM object associated selector.
+   */
+  function qs(selector) {
+    return document.querySelector(selector);
+  }
+ })();
