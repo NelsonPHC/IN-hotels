a76c3f1647f3a56237fad2d0fa58351a40c3c1e3
Socks2109 <isaacyeohcj@gmail.com>
2023-05-31 14:40:26 -0700

    changed index.html

--- public/item.js
+++ public/item.js
@@ -17,21 +17,62 @@
    * initiates page upon load
    */
   function init() {
-    getHotelName();
+    makeRequestFill();
   }
 
   /**
    * gets the name of the selected hotel
    */
-  function getHotelName() {
+  function makeRequestFill() {
     // Get the hotel name from the URL query parameter
     const urlParams = new URLSearchParams(window.location.search);
-    const hotelName = urlParams.get('hotel_nm');
+    const hotelName = "/hotels/" + urlParams.get('hid');
 
-    // Display the name on the page
-    let h1 = qs('h1');
-    h1.textContent = hotelName.trim().split('%20')
-      .join(' ');
+    fetch(hotelName)
+      .then(statusCheck)
+      .then(resp => resp.json())
+      .then(fill)
+      .catch(console.error);
+  }
+
+  function fill(response) {
+    const img = gen("img");
+    let description = gen("p");
+    const h2 = gen("h2");
+    const h1 = qs('h1');
+    const info = response[0];
+    h2.textContent = "Description:";
+    h1.textContent = info.hotelName + ", " + info.country;
+    img.src = "imgs/hotels/" + info.imageSrc;
+    img.alt = "Image of " + info.hotelName;
+    qs(".adjust > article").prepend(img);
+    const parent = id("description");
+    description.textContent = info.description;
+    parent.prepend(description);
+    parent.prepend(h2);
+  }
+
+  /**
+   * Helper function to return the response's result text if successful, otherwise
+   * returns the rejected Promise result with an error status and corresponding text
+   * @param {object} res - response to check for success/error
+   * @return {object} - valid response if response was successful, otherwise rejected
+   *                    Promise result
+   */
+  async function statusCheck(res) {
+    if (!res.ok) {
+      throw new Error(await res.text());
+    }
+    return res;
+  }
+
+  /**
+   * Returns the element that has the ID attribute with the specified value.
+   * @param {string} id - element ID.
+   * @returns {object} - DOM object associated with id.
+   */
+  function id(id) {
+    return document.getElementById(id);
   }
 
   /**
@@ -41,5 +82,14 @@
    */
   function qs(selector) {
     return document.querySelector(selector);
+  }
+
+  /**
+   * Creates a new HTML element of the specified type.
+   * @param {string} element - The type of HTML element to create.
+   * @returns {object} - The newly created HTML element.
+   */
+  function gen(element) {
+    return document.createElement(element);
   }
 })();
