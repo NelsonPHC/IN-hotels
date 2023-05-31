cac22b964dc7d861aeadaa0caa0c5bd32aac106b
Socks2109 <isaacyeohcj@gmail.com>
2023-05-31 15:17:05 -0700

    login

--- public/shared.js
+++ public/shared.js
@@ -20,6 +20,10 @@
   function init() {
     qs(".menu").addEventListener("click", clickMenu);
     qs(".right-icon").addEventListener("click", clickProfile);
+    id("login").addEventListener("submit", function(event) {
+      event.preventDefault();
+      makeRequestLogin();
+    });
   }
 
   /**
@@ -40,6 +44,47 @@
     if (!qs(".user-bar").classList.contains("hidden")) {
       qs(".user-bar").classList.toggle("hidden");
     }
+  }
+
+  function makeRequestLogin() {
+    let params = new FormData(id('login'));
+    console.log(params);
+    // const username = id("username").value;
+    // const password = id("password").value;
+    // params.append("name", username);
+    // params.append("password", password);
+    fetch("/login", {method: "POST", body: params})
+      .then(statusCheck)
+      .then(resp => resp.text())
+      .then(login)
+      .catch(console.error);
+  }
+
+  function login(response) {
+    console.log(response)
+  }
+
+  /**
+   * Helper function to return the response's result text if successful, otherwise
+   * returns the rejected Promise result with an error status and corresponding text
+   * @param {object} res - response to check for success/error
+   * @return {object} - valid response if response was successful, otherwise rejected
+   *                    Promise result
+   */
+  async function statusCheck(res) {
+    if (!res.ok) {
+      throw new Error(await res.text());
+    }
+    return res;
+  }
+
+  /**
+   * Returns the element that has the ID attribute with the specified value.
+   * @param {string} id - element ID.
+   * @returns {object} - DOM object associated with id.
+   */
+  function id(id) {
+    return document.getElementById(id);
   }
 
   /**
